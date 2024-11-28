1b10c5080326a635d0f37e9cce9fdeb4d7f16c4d
net <net@CAD1-24.igalvani.local>
2024-11-28 14:00:41 +0100

    aggiungi_prodotto

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 #inizializza l'app Flask
 app = Flask(__name__)
 
+listaSpesa = []
+
 #rotta principale
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', listaSpesa = listaSpesa)
 
-#avvio dell'app Flask
+@app.route('/aggiungi', methods=['POST']) 
+def aggiungi_prodotto():
+    elemento = request.form['elemento']
+    if elemento:
+        listaSpesa.append(elemento)
+    return redirect(url_for('home'))
+
+@app.route('/rimuovi', methods=['POST']) 
+def rimuovi_prodotto():
+    elemento = request.form['elemento']
+    if elemento:
+        listaSpesa.append(elemento)
+    return redirect(url_for('home'))
+
+#avvio dell'app Flask - Sempre mettere alla fine
 if __name__ == '__main__':
     app.run(debug=True)
-
-listaSpesa = []
